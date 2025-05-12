c801b62ac2f20d8f6d452722d5791059da1d3be7
Yana Bakanova <yanysuaBakan@yandex.ru>
2025-05-12 13:56:00 +0300

    внесены события

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,83 @@
 import './scss/styles.scss';
+import { EventEmitter } from './components/base/events';
+import { API_URL, CDN_URL } from './utils/constants';
+import { cloneTemplate, ensureElement } from './utils/utils';
+import { AppApi } from './components/common/AppApi';
+import { CardsData } from './components/common/CardsData';
+import { Page } from './components/common/Page';
+import { ICard } from './types';
+import { Card } from './components/common/Card';
+import { Modal } from './components/common/Modal';
+import { IEvents } from './components/base/events';
+
+const events = new EventEmitter();
+const api = new AppApi(API_URL, CDN_URL);
+
+// шаблоны
+
+const successTemplate = ensureElement<HTMLTemplateElement>('#success');
+const cardCatalogTemplate = ensureElement<HTMLTemplateElement>('#card-catalog');
+const cardPreviewTemplate = ensureElement<HTMLTemplateElement>('#card-preview');
+const cardBasketTemplate = ensureElement<HTMLTemplateElement>('#card-basket');
+const basketTemplate = ensureElement<HTMLTemplateElement>('#basket');
+const orderTemplate = ensureElement<HTMLTemplateElement>('#order');
+const contactsTemplate = ensureElement<HTMLTemplateElement>('#contacts');
+
+const cardsData = new CardsData(events);
+const page = new Page(document.body, events);
+const modal = new Modal(ensureElement<HTMLElement>('#modal-container'), events);
+
+// загрузка карточек с сервера
+
+api.getCardList()
+  .then((data) => {
+    cardsData.cards = data;
+    console.log(cardsData.cards);
+    events.emit('cards:loaded'); 
+  })  
+  .catch((err) => {
+    console.error('Не удалось загрузить карточки:', err);
+    events.emit('cards:load-error', err);
+});
+
+// обновление каталога товаров
+
+events.on('cards:changed', (items: ICard[]) => {
+  page.gallery = items.map(item => {
+    const card = new Card(cloneTemplate(cardCatalogTemplate), events);
+    return card.render({
+      category: item.category,
+      title: item.title,
+      image: item.image,
+      price: item.price,
+    });
+  });
+});
+
+// выбор карточки
+
+events.on('card:selected', (item: ICard) => {
+  cardsData.cardIdPreview(item);
+});
+
+// функция создания модального окна с карточкой
+
+function createPreviewCard(item: ICard) {
+
+  const card = new Card(cloneTemplate(cardPreviewTemplate), events);
+   
+   return card.render({
+    title: item.title,
+    description: item.description,
+    category: item.category,
+    image: item.image,
+    price: item.price,
+  });
+}
+
+events.on('preview:open', (item: ICard) => {
+  modal.render({
+      content: createPreviewCard(item)
+  });
+});
+
