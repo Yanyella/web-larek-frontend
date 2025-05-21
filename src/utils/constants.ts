0bbadbf2e7578f4d56a195f0f063c6d51f9b2946
Yana Bakanova <yanysuaBakan@yandex.ru>
2025-05-21 15:26:06 +0300

    внесены изменения

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,14 @@
 export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
 export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
 
-export const settings = {
+export const settings = {};
 
+// категории карточек
+
+export const categoryMap: { [key: string]: string } = {
+	'софт-скил': 'card__category_soft',
+	'хард-скил': 'card__category_hard',
+	кнопка: 'card__category_button',
+	другое: 'card__category_other',
+	дополнительное: 'card__category_additional',
 };
